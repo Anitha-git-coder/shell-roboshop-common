cc074e9f970f32f451d74f723ad9433d27f8c6e3
Anitha <anitha.pinipe14@gmail.com>
2025-11-03 13:28:31 +0530

    msg

---
+++ catalogue.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source ./common.sh
+app_name=catalogue
+app_setup
+nodejs_setup
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+VALIDATE $? "copy mongo repo"
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "install mongodb client"
+#mongoDB command not linux cmd
+INDEX=$(mongosh mongodb.anitha.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ INDEX -ne 0]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "load $app_name products"
+else 
+    echo -e "already loaded $Y skipping $N"
+fi
+
+app_restart
+
+print_total_time
--- common.sh
+++ common.sh
@@ -9,7 +9,8 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-
+START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.anitha.fun
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -28,6 +29,44 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     else
         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "disabling nodejs"
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "enabling nodejs"
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "installing nodejs"
+    npm install &>>$LOG_FILE
+    VALIDATE $? "install dependencies"
+}
+
+app_setup(){
+    mkdir -p /app 
+    VALIDATE $? "creating app directory"
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "creating $app_name application"
+    cd /app 
+    VALIDATE $? "changeing the app directory"
+    rm -rf /app/*
+    VALIDATE $? "removing existing code"
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzip $app_name"
+    }
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "copy systemctl service"
+    systemctl daemon-reload
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "enable $app_name"
+
+}
+
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "restarted catalogue"
 }
 
 print_total_time(){
